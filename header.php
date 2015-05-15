1b37335b3ca8995c06fd79663c8965bf266561dd
Period 4 <period4@ws-h093828b6mj.ad.ccsd.net>
2015-05-15 09:16:33 -0700

    Initial Commit

---
+++ header.php
@@ -0,0 +1,12 @@
+
+	<header>
+		<img src="_assets/logo.png" id="logo">
+		<h1 id="nvmd"><span class="nvmdWords"><span class="first-letters">N</span>ational</span> <span class="nvmdWords"></span><span class="first-letters">V</span>endor</span> <span class="nvmdWords"><span class="first-letters">M</span>anagement</span> <span class="nvmdWords"><span class="first-letters">D</span>epartment</span></h1>
+		<nav>          
+			<ul>
+				<a href="index.html"><li>HOME</li></a>
+				<a href="news.html"><li>NEWS & ANNOUNCEMENTS</li></a>
+				<a href="contact.html"><li>CONTACT US</li></a>
+			</ul>
+		</nav> 
+	</header>
